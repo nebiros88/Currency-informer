02e3f82e21f7677d6aeefcfd1b8c40a47cd767bb
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2020-12-13 20:56:06 +0300

    change data in serviceworker

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+let dataCacheName = 'v1';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
@@ -19,13 +21,28 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then((resp) => {
-            if(resp) {
-                return resp;
-            }
-            return fetch(event.request);
-        })
-    );
+    let dataUrl = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
+    if(event.request.url.indexOf(dataUrl) === 0) {
+        // Data handler cod will be here
+        event.respondWith(
+            fetch(event.request)
+            .then(function(response) {
+                return caches.open(dataCacheName).then(function(cache) {
+                    cache.put(event.request.url, response.clone());
+                    return response
+                });
+            })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+            .then((resp) => {
+                if(resp) {
+                    return resp;
+                }
+                return fetch(event.request);
+            })
+        );
+    }
+    
 });
